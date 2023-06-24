c976e44862d408503c7c9316b7dbbe1332844f38
2005balaji <balajisr000@gmail.com>
2023-06-25 01:31:56 +0530

    hosted node project

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-fetch('http://143.198.225.25:3000/blogs')
+let link = 'http://143.198.225.25:3000/blogs'
+
+fetch(link)
   .then(response => response.json())
   .then(data => {
     // Render the blogs here
@@ -11,14 +13,18 @@ fetch('http://143.198.225.25:3000/blogs')
       // Create HTML elements
       const keyElement = document.createElement('h2');
       const valueElement = document.createElement('p');
+      const apiLink = document.createElement('h1');
 
       // Set the text content
       keyElement.textContent = key;
       valueElement.textContent = value;
+      apiLink.textContent = link;
 
       // Append elements to container
       container.appendChild(keyElement);
       container.appendChild(valueElement);
+      container.appendChild(apiLink);
+      
     }
   })
   .catch(error => console.error(error));
